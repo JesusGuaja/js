4483c2564756e17311ab96563ef0480b031f2e8a
JesusGuaja <jesusguajardo080@gmail.com>
2021-11-23 17:06:06 -0300

    desafios

---
+++ conversor/index.js
@@ -0,0 +1,9 @@
+const valor = document.getElementById('valor');
+const select1 = document.getElementById('select1');
+const select2 = document.getElementById('select2');
+const convertir = document.getElementById('convertir');
+
+convertir.addEventListener('click',hacerConversion);
+function hacerConversion() {
+    
+}
