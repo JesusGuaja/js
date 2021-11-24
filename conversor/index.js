69b19b337cd7358ddb4ab681edd1401163015774
JesusGuaja <jesusguajardo080@gmail.com>
2021-11-24 16:38:43 -0300

    finalizando

--- conversor/index.js
+++ conversor/index.js
@@ -1,9 +1,51 @@
 const valor = document.getElementById('valor');
 const select1 = document.getElementById('select1');
 const select2 = document.getElementById('select2');
+const resultado = document.getElementById('resultado');
 const convertir = document.getElementById('convertir');
 
 convertir.addEventListener('click',hacerConversion);
 function hacerConversion() {
     
+
+
+
+
+    switch (select1.value) {
+        case "mm":
+            if (select2.value=="cm") {
+                resultado.innerHTML = valor.value/10
+            }
+            else if(select2.value=="mts") {
+                resultado.innerHTML = valor.value/100
+            }
+            else {
+                resultado.innerHTML = valor.value
+            }
+        break;
+        case "cm":
+            if (select2.value=="mm") {
+                resultado.innerHTML = valor.value*10
+            }
+            else if(select2.value=="mts") {
+                resultado.innerHTML = valor.value/100
+            }
+            else {
+                resultado.innerHTML = valor.value;
+            }
+
+        break;
+        case "mts":
+            if (select2.value=="mm") {
+                resultado.innerHTML = valor.value*1000
+            }
+            else if (select2.value=="cm") {
+                resultado.innerHTML = valor.value*100
+            }
+            else {
+                resultado.innerHTML = valor.value;
+            }
+            
+        break;
+    }
 }
