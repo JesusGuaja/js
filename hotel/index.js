02091ee7ef0911dac8ccdd748857228fab384b60
JesusGuaja <jesusguajardo080@gmail.com>
2021-11-24 16:38:37 -0300

    finalizando

---
+++ hotel/index.js
@@ -0,0 +1,8 @@
+const mensaje = document.getElementById('mensaje');
+const comentario = document.getElementById('comentario');
+var maximo = 140;
+
+comentario.addEventListener('input',actualizar);
+function actualizar() {
+    if (comentario.nodeValue.length>) {}
+}
